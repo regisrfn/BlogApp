a4a43c6ed6b18f0cca0e94da239523502a1cd295
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-03-09 14:48:08 -0300

    Set up deploy aplication running through file server.js

--- client/config/index.js
+++ client/config/index.js
@@ -72,5 +72,12 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
+  },
+
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }
   }
 }
