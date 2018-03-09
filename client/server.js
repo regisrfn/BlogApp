5dc1723618310655ba0e6f18f0ae34b6a7051b29
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-03-09 16:16:24 -0300

    included connect-history-api-fallback

--- client/server.js
+++ client/server.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 var serveStatic = require('serve-static')
+var history = require('connect-history-api-fallback')
 
 const app = express()
 
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(history())
 
 app.use(serveStatic(__dirname + '/dist'))
 
