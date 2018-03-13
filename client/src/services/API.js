0c13506325c50085ea8e6888c1a040bdb31707e4
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-03-13 11:08:02 -0300

    Changed database url

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://blogapp.eastus.cloudapp.azure.com:8081'
-    // baseURL: 'http://localhost:8081'
+    // baseURL: 'http://blogapp.eastus.cloudapp.azure.com:8081'
+    baseURL: 'http://localhost:8081'
   })
 }
