059b5f9482b50c66f9a932577b1d12f4ea612a4e
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-03-13 18:48:05 -0300

    Hosting app on azure vm

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -3,6 +3,5 @@ import axios from 'axios'
 export default () => {
   return axios.create({
     baseURL: 'http://app-server.westus.cloudapp.azure.com:8081'
-    // baseURL: 'http://localhost:8081'
   })
 }
