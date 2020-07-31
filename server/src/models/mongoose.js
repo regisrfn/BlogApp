0e14488da25e08836deac9a5a342e5ea5a3480d7
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-07-31 14:46:29 -0300

    init

--- server/src/models/mongoose.js
+++ server/src/models/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app')
+
 module.exports = mongoose
