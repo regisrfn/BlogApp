8e0ce15c3663a84ffbcaf2dddf57ed433debb1c8
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-07-31 12:12:14 -0300

    Updating packges

--- server/src/models/mongoose.js
+++ server/src/models/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app')
-
+mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 module.exports = mongoose
