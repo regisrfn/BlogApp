9a810012742648c558702a0eb458ba77413dc06c
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-21 16:17:59 -0300

    Update pack

--- server/src/models/mongoose.js
+++ server/src/models/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app')
+mongoose.connect(process.env.DATABASE_API || 'mongodb://localhost/blog_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, useFindAndModify: false
+})
 
 module.exports = mongoose
